3cdaf8f5bbfef07aeb6b80c3455421752bdd9abe
DESKTOP-58045T5\Durgesh <brijmohanpatel299@gmail.com>
2021-01-25 15:25:44 +0530

    First Fresh Commit

---
+++ src/main/java/LachelnDCMSBackend/LachelnDCMSBackend/App.java
@@ -0,0 +1,13 @@
+package LachelnDCMSBackend.LachelnDCMSBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
